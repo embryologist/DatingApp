0b1ce413faa08889a0320962d5b9894056fa8ea2
embryologist <aamaizar@gmail.com>
2025-07-31 19:38:40 -0700

    Initial

---
+++ API/Data/DbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Formats.Tar;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+   
+
+
+}
---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+   });
+    
+// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi(); removed by me
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// } removed by me
+
+// app.UseHttpsRedirection();removed by me
+
+// app.UseAuthorization();removed by me
+
+app.MapControllers();
+
+app.Run();
