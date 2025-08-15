22bbaaf671b13a75c335b00cc6d144d646a2d2a8
embryologist <aamaizar@gmail.com>
2025-08-14 22:29:32 -0700

    adding login meth

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ServiceService } from '../../core/services/accounts/service.service';
 
@@ -11,16 +11,23 @@ import { ServiceService } from '../../core/services/accounts/service.service';
 export class Nav {
   private accountsService = inject(ServiceService);
   protected creds: any = {}
+  protected isLoggedIn = signal(false); 
 
   login() {
     this.accountsService.login(this.creds).subscribe({
       next: (response) => {
         console.log('Login successful', response);
+        this.isLoggedIn.set(true);
+        alert('Login successful! Welcome back.');
       },
       error: (error) => {
         console.error('Login failed', error);
         alert('Login failed. Please check your credentials and try again.' + ' ' + error.message);
       }
     })
+  }
+
+  logout() {
+    this.isLoggedIn.set(false);
   }
 }
